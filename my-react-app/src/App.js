db72ecd98d8d06d31d7ed9da13dcd0506ae22aec
SHAHEER MUZAFFAR <edewvv@gmail.com>
2026-06-29 17:16:04 +0500

    built the first component in React

---
+++ my-react-app/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header() {
+  return <h1>Welcome to My React App</h1>;
+}
+function App() {
+ return <Header />; 
+}
+
+export default App;
